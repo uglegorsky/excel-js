7b6e3dcd328b151a99f97f62bddf02f66fd28c44
Dmitry Voronin <developer.dmitry.voronin@gmail.com>
2020-05-16 23:43:51 +0300

    feat: resizing table elements

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,12 +1,24 @@
-/* eslint-disable max-len */
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
-
+import { resizeHandler } from './table.resize';
+import { shoudResize } from './table.functions';
 export class Table extends ExcelComponent {
   static className = 'table';
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
 
   toHTML() {
     // The function parameter takes value counts of rows
     return createTable(20);
+  }
+
+  onMousedown(event) {
+    if (shoudResize(event)) {
+      resizeHandler(this.$root, event);
+    }
   }
 }
